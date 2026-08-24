d2796a2fc68ceca74cf1c98480eec1a352806b2b
vbnm7180 <vbnm7180@mail.ru>
2026-08-24 20:17:04 +0400

    Настройка конфигураций: Vite, TS, ESLint, Prettier

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,7 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import prettierConfig from 'eslint-config-prettier';
 
 export default defineConfig([
   globalIgnores(['dist']),
@@ -11,13 +12,81 @@ export default defineConfig([
     files: ['**/*.{ts,tsx}'],
     extends: [
       js.configs.recommended,
-      tseslint.configs.recommended,
+      tseslint.configs.strictTypeChecked,
+      tseslint.configs.stylisticTypeChecked,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
     ],
+      rules: {
+    // Disable the base rule as it can report incorrect errors
+    "default-param-last": "off",
+    "@typescript-eslint/default-param-last": "error",
+    //todo: delete if needed
+    "@typescript-eslint/explicit-function-return-type": "error",
+    //todo: delete if needed
+    "@typescript-eslint/explicit-module-boundary-types": "error",
+        // Disable the base rule as it can report incorrect errors
+    "max-params": "off",
+    "@typescript-eslint/max-params": ["error", { "max": 3 }],
+        "@typescript-eslint/member-ordering": [
+      "error",
+      { "default": ["signature", "constructor", "method", "field"] },
+            {
+        "default": {
+          "order": 'natural',
+        },
+      },
+    ],
+        // Disable the base rule as it can report incorrect errors
+    "no-magic-numbers": "off",
+    "@typescript-eslint/no-magic-numbers": ["error", {
+      "ignore": [1],
+      "ignoreArrayIndexes": true,
+      "ignoreDefaultValues": true,
+      "ignoreClassFieldInitialValues": true,
+      "ignoreReadonlyClassProperties": true,
+      "ignoreEnums": true,
+      "ignoreNumericLiteralTypes": true,
+      "ignoreTypeIndexes": true
+    }],
+    "@typescript-eslint/no-unnecessary-parameter-property-assignment": "error",
+    "@typescript-eslint/no-unnecessary-qualifier": "error",
+        // Disable the base rule as it can report incorrect errors
+    "no-unused-private-class-members": "off",
+    "@typescript-eslint/no-unused-private-class-members": "error",
+        // Disable the base rule as it can report incorrect errors
+    "no-use-before-define": "off",
+    "@typescript-eslint/no-use-before-define": "error",
+    "@typescript-eslint/no-useless-empty-export": "error",
+    "@typescript-eslint/prefer-enum-initializers": "error",
+    "@typescript-eslint/prefer-readonly": "error",
+    "@typescript-eslint/strict-boolean-expressions": "error",
+      "@typescript-eslint/naming-convention": [
+    "error",
+    {
+      "selector": "enum",
+      "format": ["PascalCase"],
+    },
+          // 1. Базовое правило: для всех переменных разрешены camelCase и UPPER_CASE
+      {
+        selector: "variable",
+        modifiers: ["const"],
+        format: ["camelCase", "PascalCase", "UPPER_CASE"],
+      },
+      // 2. Основное правило: требовать UPPER_CASE для const на верхнем уровне
+      {
+        selector: "variable",
+        modifiers: ["const", "global"],
+        format: ["UPPER_CASE"],
+      },
+  ]
+  },
     languageOptions: {
-      ecmaVersion: 2020,
       globals: globals.browser,
+          parserOptions: {
+      projectService: true,
+    },
     },
   },
+  prettierConfig,
 ])
--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,7 @@ import tailwind from '@tailwindcss/vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwind()],
+  build: {
+    sourcemap: true
+  }
 })
